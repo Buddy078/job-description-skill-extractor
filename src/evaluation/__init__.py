f425898c9ca6aea8309c625e09fe2578d8439128
Rogith K <rogithlearning078@gmail.com>
2026-09-15 13:29:00 +0530

    Remove old project files

--- src/evaluation/__init__.py
+++
@@ -1,9 +0,0 @@
-from .metrics import normalize_entity_name, calculate_set_metrics, evaluate_extraction
-from .comparator import PipelineComparator
-
-__all__ = [
-    "normalize_entity_name",
-    "calculate_set_metrics",
-    "evaluate_extraction",
-    "PipelineComparator",
-]
